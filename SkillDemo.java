5dae337ef338331332d2b40e7d305ad59926053c
Pranav Nair <cs15lsp22agu@ieng6-201.ucsd.edu>
2022-05-08 12:42:18 -0700

    modify line

--- SkillDemo.java
+++ SkillDemo.java
@@ -2,7 +2,7 @@ public class SkillDemo {
 
     public static int factorial(int x) {
         int output = 1;
-        for (int i = 1; i <= x; i++) {
+        for (int i = (0+1); i <= x; i++) {
             output *= i;
         }
         return output;
